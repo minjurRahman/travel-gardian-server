b70adc76fdff1046e9567d74a4b89525ba7e540c
Minjur <minjur.rahaman99@gmail.com>
2022-11-09 10:23:47 +0600

    service data and get a data from service done

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 
 const app = express();
@@ -18,12 +18,39 @@ app.get('/', (req, res) =>{
 //Database connection work
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.bjaguop.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
 
+//CRUD Operation
+async function run(){
+    try{
+        const serviceCollection = client.db('travelGuardian').collection('travelArea');
+
+        app.get('/services', async(req, res) =>{
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        })
+
+        app.get('/services/:id', async(req, res) =>{
+            const id = req.params.id;
+            const query = { _id: ObjectId(id)};
+            const service = await serviceCollection.findOne(query);
+            req.send(service);
+        })
+
+
+
+
+    }
+    catch(error){
+        console.log(error.name, error.message.bold, error.stack)
+    }
+    finally{
+
+    }
+}
+
+run();
 
 
 
